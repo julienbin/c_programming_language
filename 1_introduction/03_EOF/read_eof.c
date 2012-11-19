23545f10498848f30202035b275202da892f97e8
julien <julienbin@gmail.com>
2012-11-19 21:52:17 +0800

    read stream and count words and skip blank

--- 1_introduction/03_EOF/read_eof.c
+++ 1_introduction/03_EOF/read_eof.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 main() {
   printf("EOF is: ");
   putchar(EOF);
   printf("\nEOF int value is: %d.\n", EOF);
   
-  int c;
+  int c, state, skip;
+  long nc, nl, nw;
+  state = OUT;
+  nc = nl = nw = 0;
+  skip = 0;
   while ((c = getchar()) != EOF) {
-    putchar(c);
+    ++nc;
+    if (!skip)
+      putchar(c);
+    else if (c != ' ')
+      putchar(c);
+
+    if (c == ' ')
+      skip = 1;
+    else
+      skip = 0;
+    
+    if (c == '\n') {
+      ++nl;
+    }
+    if (c == ' ' || c == '\n' || c == '\t')
+      state = OUT;
+    else if (state == OUT) {
+      state = IN;
+      ++nw;
+    }
   }
+  printf("%ld chars %ld words %ld lines.\n", nc, nw, nl);
 }
